db09717ded36a22619d342e810be8e890ac72c4c
Juan <juanmetchart@gmail.com>
2025-09-27 16:17:00 -0300

    Handle login

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
-import { useState } from 'react'
-// @ts-ignore
+import { useState, useEffect } from 'react'
 import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google';
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Badge } from './components/ui/badge';
+import { BadgeCheckIcon } from 'lucide-react';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [userName, setUserName] = useState<string | null>(null);
+
+  useEffect(() => {
+    const storedName = localStorage.getItem('userName');
+    if (storedName) {
+      setUserName(storedName);
+    }
+  }, []);
 
   // Función para enviar el token al backend
-  const handleGoogleLoginSuccess = async (credentialResponse) => {
+  const handleGoogleLoginSuccess = async (credentialResponse: any) => {
     const token = credentialResponse.credential;
     try {
       const response = await fetch('http://localhost:3000/auth/google/login', {
@@ -20,44 +26,39 @@ function App() {
         body: JSON.stringify({ token }),
       });
       const data = await response.json();
-      // Puedes manejar la respuesta aquí
+      if (data) {
+        setUserName(data.user.name);
+        localStorage.setItem('userName', data.user.name);
+      }
       console.log('Login backend response:', data);
     } catch (error) {
       console.error('Error enviando token al backend:', error);
     }
   };
 
+  function handleLogout(): void {
+    setUserName(null);
+    localStorage.removeItem('userName');
+  }
+
   return (
-    <GoogleOAuthProvider clientId="65738319816-84n4osqr38kdjhqdrr7q0a0id2d56gre.apps.googleusercontent.com">
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <div style={{ marginTop: '2rem' }}>
+        {userName ? (
+          <>
+          <Badge variant="secondary"
+          className="bg-blue-500 text-white dark:bg-blue-600" onClick={handleLogout}>{userName} <BadgeCheckIcon /></Badge>
+            </>
+        ) : (
+          <GoogleOAuthProvider clientId="65738319816-84n4osqr38kdjhqdrr7q0a0id2d56gre.apps.googleusercontent.com">
         <GoogleLogin
           onSuccess={handleGoogleLoginSuccess}
           onError={() => {
             console.log('Login Failed');
           }}
         />
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </GoogleOAuthProvider>
+        )}
+      </div>
   );
 }
 
