ffd6d2fb243a2e4b344eeabd48ae470616d6e8be
Juan <juanmetchart@gmail.com>
2025-09-29 17:03:59 -0300

    Environment variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,11 @@
+declare global {
+  interface ImportMeta {
+    env: {
+      VITE_BACKEND_URL: any;
+      VITE_GOOGLE_CLIENT_ID: string;
+    };
+  }
+}
 import { useState, useEffect } from 'react'
 import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google';
 import './App.css'
@@ -17,7 +25,7 @@ function App() {
   const handleGoogleLoginSuccess = async (credentialResponse: any) => {
     const token = credentialResponse.credential;
     try {
-      const response = await fetch('http://localhost:3000/auth/google/login', {
+      const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/auth/google/login`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -48,7 +56,7 @@ function App() {
           className="bg-blue-500 text-white dark:bg-blue-600" onClick={handleLogout}>{userCredential.name} <BadgeCheckIcon /></Badge>
             </>
         ) : (
-          <GoogleOAuthProvider clientId="65738319816-84n4osqr38kdjhqdrr7q0a0id2d56gre.apps.googleusercontent.com">
+          <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
         <GoogleLogin
           onSuccess={handleGoogleLoginSuccess}
           onError={() => {
